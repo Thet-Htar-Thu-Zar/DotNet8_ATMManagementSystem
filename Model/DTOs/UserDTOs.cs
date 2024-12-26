bda44ef299d986e1ee7a16a273b81f85920f2f95
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-26 13:58:30 +0630

    CreateUserDTO

---
+++ Model/DTOs/UserDTOs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DTOs
+{
+    public class CreateUserDTOs
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public decimal? Amount { get; set; }
+
+    }
+}
---
+++ Model/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using Model.ApplicationConfig;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entities
+{
+    [Table("Tbl_ATMUser")]
+    public class Users : Common
+    {
+        [Key]
+        public Guid UserID { get; set; } = new Guid();
+        public string? Password { get; set; }
+        public string? UserName { get; set; }
+        public string? AccountNo { get; set;}
+        public decimal? Amount { get; set;}
+    }
+}
