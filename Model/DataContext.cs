77ec85141fe137b7bf52b0d8673559f816cf9e80
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-26 14:02:54 +0630

    CreateDbset

--- Model/DataContext.cs
+++ Model/DataContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Model
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+
+namespace Model
 {
-    internal class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Users> User { get; set; }
     }
 }
