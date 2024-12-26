9845ef8cf1cf16091683fc9320422901ed7f662c
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-26 13:49:32 +0630

    -

--- ATMManagementSystem/Program.cs
+++ ATMManagementSystem/Program.cs
@@ -1,9 +1,13 @@
+using Model.ApplicationConfig;
+
 var builder = WebApplication.CreateBuilder(args);
 var appSettings = new Appsetting();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.GetSection("AppSettings").Bind(appSettings);
+ServiceManager.SetServicesInfo(builder.Services, appSettings);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
