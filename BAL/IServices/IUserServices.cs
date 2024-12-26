add9909b7c86478aa8b2ac70a9de2f9f1389069e
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-26 16:01:21 +0630

    Add UserService

---
+++ BAL/IServices/IUserServices.cs
@@ -0,0 +1,14 @@
+﻿using Model.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.IServices
+{
+    public interface IUserServices
+    {
+        Task CreateUser(CreateUserDTOs inputModel);
+    }
+}
