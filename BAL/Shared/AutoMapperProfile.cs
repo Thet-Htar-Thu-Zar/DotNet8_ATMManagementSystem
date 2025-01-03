582feedc4cc1ad61fe2bb4ec19561774c345c0a1
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-03 14:08:48 +0630

    AddJwtTokenProvider

---
+++ BAL/Shared/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Model.DTOs;
+using Model.Entities;
+
+namespace ATMManagementSystem
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Users, CreateUserDTOs>().ReverseMap();
+
+        }
+
+    }
+}
