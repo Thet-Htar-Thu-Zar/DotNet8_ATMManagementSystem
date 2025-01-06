6017bd371e2183c42cfe393072b93fde4cf83acf
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-06 15:35:51 +0630

    fix code

---
+++ BAL/IServices/ITransactionServices.cs
@@ -0,0 +1,18 @@
+﻿using Model.DTOs;
+using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.IServices
+{
+    public interface ITransactionServices
+    {
+        Task<IEnumerable<Transactions>> GetAllTransactions();
+        Task Deposit(CreateTransactionDTOs inputModel);
+        Task Withdraw(CreateTransactionDTOs inputModel);
+        Task<IEnumerable<Transactions>> GetTransactionsById(Guid UserId);
+    }
+}
