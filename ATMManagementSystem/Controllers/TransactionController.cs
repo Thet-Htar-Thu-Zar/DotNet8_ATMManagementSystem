6c8b0341bf62ce44a23c2a1cfe8f89fb2c2942d1
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-06 10:56:09 +0630

    add transaction

---
+++ ATMManagementSystem/Controllers/TransactionController.cs
@@ -0,0 +1,15 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATMManagementSystem.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class TransactionController : ControllerBase
+    {
+    }
+}
