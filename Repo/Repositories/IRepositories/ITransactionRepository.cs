5fd2e73660f0689a2709d7aef54eac44192c3943
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-07 09:34:38 +0630

    fix code

--- Repo/Repositories/IRepositories/ITransactionRepository.cs
+++ Repo/Repositories/IRepositories/ITransactionRepository.cs
@@ -1,10 +1,4 @@
 ﻿using Model.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
 
 namespace Repo.Repositories.IRepositories
 {
