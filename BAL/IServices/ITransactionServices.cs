0477afe4b15f11bac9fe2f37d1cb504f7b66ab67
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-07 09:30:48 +0630

    fix code

--- BAL/IServices/ITransactionServices.cs
+++ BAL/IServices/ITransactionServices.cs
@@ -1,10 +1,5 @@
 ﻿using Model.DTOs;
 using Model.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BAL.IServices
 {
